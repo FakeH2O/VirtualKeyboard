20e3692caa6dc4393c1e3d7b226532267adf9877
jayvanandel07 <j.vanandel07@gmail.com>
2022-10-18 09:49:58 +0530

    optimize

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import numpy as np
 import time
 import cv2
 # from cvzone.HandTrackingModule import HandDetector
-import pyglet
+import pygame
+
 import HandTrackingModule as htm
 
 cap =cv2.VideoCapture(0)
@@ -13,7 +14,7 @@ cap.set(3,1280)
 cap.set(4,720)
 
 detector = htm.handDetector()
-window = pyglet.window.Window()
+# window = pyglet.window.Window()
 # detector =HandDetector(detectionCon=0.8)
 
 keys=[["C","D",'E',"F","G","A","B","C","D","E","F","G","A","B"],["C#","D#","F#","G#","A#","C#","D#","F#","G#","A#"]]
@@ -28,44 +29,60 @@ buttonList=[]
 for i in range(len(keys)):
     for j,key in enumerate(keys[i]):   
         if i==0:
-            buttonList.append(Button([(j*70)+j*5+20,80],key,[70,200],(255,255,255)))
+            buttonList.append(Button([(j*70)+j*6+30,80],key,[70,200],(255,255,255)))
         else:
-            buttonList.append(Button([(j*70)+35+j*30+20,80],key,[70,100],(0,0,0)))    
+            buttonList.append(Button([(j*70)+j*31+65,80],key,[70,100],(0,0,0)))    
 
 def playkeys(button):
     if button.text=="A":
-            
-        effectA=pyglet.resource.media("assets/A.wav",streaming=False)
-        effectA.play()
+        
+        pygame.mixer.Sound("assets/A.wav").play()
+        
+        time.sleep(0.08)
+        pygame.mixer.Sound("assets/A.wav").stop()
+        
                 
                 
     elif button.text=="B":
             
-        effectB=pyglet.resource.media("assets/B.wav",streaming=False)
-        effectB.play()
+        pygame.mixer.Sound("assets/B.wav").play()
+        
+        time.sleep(0.08)
+        pygame.mixer.Sound("assets/B.wav").stop()
                 
     elif button.text=="C":
             
-        effectC=pyglet.resource.media("assets/C.wav",streaming=False)
-        effectC.play()
+        pygame.mixer.Sound("assets/C.wav").play()
+        
+        time.sleep(0.08)
+        pygame.mixer.Sound("assets/C.wav").stop()
     elif button.text=="D":
             
-        effectD=pyglet.resource.media("assets/D.wav",streaming=False)
-        effectD.play()
+        pygame.mixer.Sound("assets/D.wav").play()
+        
+        time.sleep(0.08)
+        pygame.mixer.Sound("assets/D.wav").stop()
+        
     elif button.text=="E":
             
-        effectE=pyglet.resource.media("assets/E.wav",streaming=False)
-        effectE.play()
+        pygame.mixer.Sound("assets/E.wav").play()
+        
+        time.sleep(0.08)
+        pygame.mixer.Sound("assets/E.wav").stop()
         
 
     elif button.text=="F":
             
-        effectF=pyglet.resource.media("assets/F.wav",streaming=False)
-        effectF.play()
+        pygame.mixer.Sound("assets/F.wav").play()
+        
+        time.sleep(0.08)
+        pygame.mixer.Sound("assets/F.wav").stop()
     elif button.text=="G":
             
-        effectG=pyglet.resource.media("G.wav",streaming=False)
-        effectG.play()                  
+        pygame.mixer.Sound("assets/G.wav").play()
+        
+        time.sleep(0.08)
+        pygame.mixer.Sound("assets/G.wav").stop()
 
 
 def drawAll(img,buttonList):
@@ -83,11 +100,13 @@ def drawAll(img,buttonList):
     
         
 
-
+count=0
 
 while True:
     success,img=cap.read()
+    pygame.init()
     img=cv2.flip(img,1)
+
     img = detector.findHands(img, draw=True )
     lmList = detector.findPosition(img, draw=False)
     img=drawAll(img,buttonList)
@@ -97,14 +116,19 @@ while True:
             w,h=button.size
             for f in [4,8,12,16,20]:
                 if x<lmList[f][1]<x+w and y<lmList[f][2]<y+h:
+                    
                     l,_,_=detector.findDistance(lmList[f],lmList[f-3],img)
-                    print(l)
-                    if l<120:
+                    print(count)
+                    count+=1
+                    if l<120 and (count==0 or count>50 and count<80):
                         try:
                             playkeys(button)
-                        except:
-                            print("error")
-                            
+                            count=0
+                        except Exception as e:
+                            print(e)
+                    if count>80:
+                        count=0
+                    
                             
 
     key=cv2.waitKey(1)
